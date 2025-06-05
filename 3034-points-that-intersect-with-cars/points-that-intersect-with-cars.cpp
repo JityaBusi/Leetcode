52cbd76b45f739ab47374d948e8902cd237c6f6d
JityaBusi <119723522+JityaBusi@users.noreply.github.com>
2025-06-05 10:58:28 +0530

    Time: 5 ms (27.97%) | Memory: 30.4 MB (16.52%) - LeetSync

---
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numberOfPoints(vector<vector<int>>& nums) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=nums[i][0];j<=nums[i][1];j++)
+            {
+                mp[j]++;
+            }
+        }
+        return mp.size();
+    }
+};
